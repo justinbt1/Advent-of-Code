56e661e87982dd33c1260c188ce483894435bea1
Justin <85031828+justinbt1@users.noreply.github.com>
2022-12-03 10:48:00 +0000

    Added code for day 1

--- 2022/day_1-calorie_counting.py
+++ 2022/day_1-calorie_counting.py
@@ -1,12 +1,22 @@
 
-def question_1_part_a():
-    max_calories = 0
+def get_top_3_elves():
     elf_calories_sum = 0
-
-    with open('../data/1A.txt', 'rt') as data_file:
+    totals = []
+    with open('data/1A.txt', 'rt') as data_file:
         for row in data_file:
-            if row:
-                print(row)
+            if row.strip():
+                elf_calories_sum += int(row)
+            else:
+                totals.append(elf_calories_sum)
+                elf_calories_sum = 0
+
+    totals.append(elf_calories_sum)
+    totals.sort(reverse=True)
+
+    return totals[:3]
+
 
 if __name__ == '__main__':
-    question_1_part_a()
+    elves_highest_calorie = get_top_3_elves()
+    print(f'Question 1 part A: {elves_highest_calorie[0]}')
+    print(f'Question 1 part A: {sum(elves_highest_calorie)}')
