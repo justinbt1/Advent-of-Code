c28e191b126ab91b3236a615f414a8d198acd0d8
robert-drop-table <85031828+robert-drop-table@users.noreply.github.com>
2022-12-04 22:40:14 +0000

    Moved dictionary out of functions

--- 2022/day_2-rock_paper_scissors.py
+++ 2022/day_2-rock_paper_scissors.py
@@ -1,11 +1,10 @@
 
 
-def part_1(data):
+def part_1(data, mapping):
     total_score = 0
-    plays_as_ints = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
     for line in data:
-        opponent = plays_as_ints[line[0]]
-        response = plays_as_ints[line[1]]
+        opponent = mapping[line[0]]
+        response = mapping[line[1]]
         total_score += response
 
         if opponent == response:
@@ -18,22 +17,21 @@ def part_1(data):
     print(total_score)
 
 
-def part_2(data):
+def part_2(data, mapping):
     total_score = 0
-    plays_as_ints = {'A': 1, 'B': 2, 'C': 3}
     for line in data:
 
         if line[1] == 'X':
-            response = plays_as_ints[line[0]] - 1
+            response = mapping[line[0]] - 1
             if response == 0:
                 response = 3
             total_score += response
 
         elif line[1] == 'Y':
-            total_score += 3 + plays_as_ints[line[0]]
+            total_score += 3 + mapping[line[0]]
 
         elif line[1] == 'Z':
-            response = plays_as_ints[line[0]] + 1
+            response = mapping[line[0]] + 1
             if response == 4:
                 response = 1
             total_score += 6 + response
@@ -45,5 +43,6 @@ if __name__ == '__main__':
     with open('data/2.txt', 'rt') as data_file:
         round_data = [line.strip().split() for line in data_file]
 
-    part_1(round_data)
-    part_2(round_data)
+    hand_int_mappings = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
+    part_1(round_data, hand_int_mappings)
+    part_2(round_data, hand_int_mappings)
