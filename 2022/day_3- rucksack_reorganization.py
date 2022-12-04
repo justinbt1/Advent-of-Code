045d37ec0136e3dccddeb548241f385ef68c03ff
Justin <85031828+justinbt1@users.noreply.github.com>
2022-12-04 23:29:32 +0000

    Added day 3 part 1 code

--- 2022/day_3- rucksack_reorganization.py
+++ 2022/day_3- rucksack_reorganization.py
@@ -1,13 +1,20 @@
+import string
+
 
 def part_1(rucksack_items):
+    chars = string.ascii_lowercase + string.ascii_uppercase
+    char_priorities = {char: i + 1 for i, char in enumerate(chars)}
+    total_priority = 0
+
     for items in rucksack_items:
         n_items = int(len(items) / 2)
         rucksack_1 = set(items[:n_items])
         rucksack_2 = set(items[n_items:])
         repeat_char = rucksack_1 & rucksack_2
-        print(repeat_char)
-        print(ord(repeat_char.pop()))
+        priority = char_priorities[repeat_char.pop()]
+        total_priority += priority
 
+    print(total_priority)
 
 
 if __name__ == '__main__':
