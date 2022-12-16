52ce455ae11ec6153f3f37ed771eba07885e3b42
Justin <85031828+justinbt1@users.noreply.github.com>
2022-12-16 11:25:03 +0000

    Added code for day 7 part 2

--- 2022/day_7-no_space_left_on_device.py
+++ 2022/day_7-no_space_left_on_device.py
@@ -3,20 +3,17 @@ class DirectoryNode:
     def __init__(self, name, parent):
         self.name = name
         self.size = 0
+        self.files = set()
         self.children = {}
         self.parent = parent
 
-
-def sum_directory(tree):
-    for child in tree.children:
-        tree.size += sum_directory(tree.children[child])
-
-    print(tree.name, tree.size)
-
-    return tree.size
+    def add_file(self, file, size):
+        if file not in self.files:
+            self.files.add(file)
+            self.size += int(size)
 
 
-def tree_builder(cli_lines):
+def build_tree(cli_lines):
     current_node = DirectoryNode(name='/', parent=None)
 
     for line in cli_lines:
@@ -33,16 +30,46 @@ def tree_builder(cli_lines):
                 parent=current_node
             )
         else:
-            current_node.size += int(line[0])
+            current_node.add_file(line[1], line[0])
+
+    while current_node.name != '/':
+        current_node = current_node.parent
 
     return current_node
 
 
+def walk_tree(tree, sizes):
+    for child in tree.children:
+        child_size, sizes = walk_tree(tree.children[child], sizes)
+        tree.size += child_size
+    sizes.append(tree.size)
+    return tree.size, sizes
+
+
+def part_one(file_size_list):
+    small_directories = 0
+    for file in file_size_list:
+        if file > 100000:
+            break
+
+        small_directories += file
+
+    print(f'Part 1: {small_directories}')
+
+
+def part_two(file_size_list):
+    space_needed = 30000000 - (70000000 - root_size)
+    for file in size_list:
+        if file >= space_needed:
+            print(f'Part 2: {file}')
+            break
+
+
 if __name__ == '__main__':
     with open('data/7.txt', 'rt') as input_file:
-        file_tree = tree_builder(input_file)
+        file_tree = build_tree(input_file)
 
-    while file_tree.name != '/':
-        file_tree = file_tree.parent
-    
-    sum_directory(file_tree)
+    root_size, size_list = walk_tree(file_tree, [])
+    size_list.sort()
+    part_one(size_list)
+    part_two(size_list)
