9743e2505b3c99ed5ac27763a3a897ef1c6c5ccb
Justin <85031828+justinbt1@users.noreply.github.com>
2022-12-17 11:14:49 +0000

    Add partially complete day 8 code

--- 2022/day_8-treetop_tree_house.py
+++ 2022/day_8-treetop_tree_house.py
@@ -8,39 +8,35 @@ def create_grid():
     return grid
 
 
-def walk_rows(row, i, results):
-    previous_tree = row[0]
-    for j in range(1, len(row), 1):
-        if row[j] > previous_tree:
-            results[i][j] = 1
-            previous_tree = row[j]
-
-    previous_tree = row[-1]
-    for j in range(len(row) - 1, 0, -1):
+def row_walker(row, i, results, first_row, start, end, step_direction):
+    previous_tree = row[first_row]
+    for j in range(start, end, step_direction):
         if row[j] > previous_tree:
             results[i][j] = 1
             previous_tree = row[j]
 
 
-def brute_force_visibility_scan(grid, results):
+def walk_rows(grid, results):
     for i in range(1, len(grid) - 1):
         results[i][0] = 1
         results[i][-1] = 1
-        walk_rows(grid[i], i, results)
+        row_walker(grid[i], i, results, 0, 1, len(grid[i]) - 1, 1)
+        row_walker(grid[i], i, results, -1, len(grid[i]) - 1, 1, -1)
 
+
+def walk_columns(grid, results, first_row, start, end, step_direction):
     for j in range(1, len(grid[0]) - 1):
-        previous_tree = grid[0][j]
-        for i in range(1, len(grid) - 1):
+        previous_tree = grid[first_row][j]
+        for i in range(start, end, step_direction):
             if grid[i][j] > previous_tree:
                 results[i][j] = 1
                 previous_tree = grid[i][j]
 
-    for j in range(1, len(grid[0]) - 1):
-        previous_tree = grid[-1][j]
-        for i in range(len(grid) - 1, 1, -1):
-            if grid[i][j] > previous_tree:
-                results[i][j] = 1
-                previous_tree = grid[i][j]
+
+def brute_force_visibility_scan(grid, results):
+    walk_rows(grid, results)
+    walk_columns(grid, results, 0, 1, len(grid) - 1, 1)
+    walk_columns(grid, results, -1, len(grid) - 1, 1, -1)
 
     total = 0
     for r in results:
