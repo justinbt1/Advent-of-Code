a6d01d5488c4b178778d90a1157ac03fab65d590
Justin <85031828+justinbt1@users.noreply.github.com>
2022-12-17 21:18:34 +0000

    Add partially complete day 8 code

--- 2022/day_8-treetop_tree_house.py
+++ 2022/day_8-treetop_tree_house.py
@@ -1,4 +1,3 @@
-
 def create_grid():
     grid = []
     with open('data/8.txt', 'rt') as input_file:
@@ -8,45 +7,39 @@ def create_grid():
     return grid
 
 
-def create_results_grid(width, height):
-    results_grid = [[1 for _ in range(width)]]
-    for _ in range(height - 2):
-        results_grid.append([0 for _ in range(width)])
+def walk_rows(row, i, results):
+    previous_tree = row[0]
+    for j in range(1, len(row), 1):
+        if row[j] > previous_tree:
+            results[i][j] = 1
+            previous_tree = row[j]
 
-    results_grid += [[1 for _ in range(width)]]
-
-    return results_grid
-
-
-def row_walker(row, i, results, first_row, start, end, step_direction):
-    previous_tree = row[first_row]
-    for j in range(start, end, step_direction):
+    previous_tree = row[-1]
+    for j in range(len(row) - 1, 0, -1):
         if row[j] > previous_tree:
             results[i][j] = 1
             previous_tree = row[j]
 
 
-def walk_rows(grid, results):
+def brute_force_visibility_scan(grid, results):
     for i in range(1, len(grid) - 1):
         results[i][0] = 1
         results[i][-1] = 1
-        row_walker(grid[i], i, results, 0, 1, len(grid[i]) - 1, 1)
-        row_walker(grid[i], i, results, -1, len(grid[i]) - 1, 1, -1)
+        walk_rows(grid[i], i, results)
 
-
-def walk_columns(grid, results, first_row, start, end, step_direction):
     for j in range(1, len(grid[0]) - 1):
-        previous_tree = grid[first_row][j]
-        for i in range(start, end, step_direction):
+        previous_tree = grid[0][j]
+        for i in range(1, len(grid) - 1):
             if grid[i][j] > previous_tree:
                 results[i][j] = 1
                 previous_tree = grid[i][j]
 
-
-def brute_force_visibility_scan(grid, results):
-    walk_rows(grid, results)
-    walk_columns(grid, results, 0, 1, len(grid) - 1, 1)
-    walk_columns(grid, results, -1, len(grid) - 1, 1, -1)
+    for j in range(1, len(grid[0]) - 1):
+        previous_tree = grid[-1][j]
+        for i in range(len(grid) - 1, 1, -1):
+            if grid[i][j] > previous_tree:
+                results[i][j] = 1
+                previous_tree = grid[i][j]
 
     total = 0
     for r in results:
@@ -57,5 +50,9 @@ def brute_force_visibility_scan(grid, results):
 
 if __name__ == '__main__':
     tree_grid = create_grid()
-    results_grid = create_results_grid(len(tree_grid[0]), len(tree_grid))
+    results_grid = [[1 for i in range(len(tree_grid[1]))]]
+    results_grid += [
+        [0 for i in range(len(tree_grid[0]))] for i in range(len(tree_grid) - 2)
+    ]
+    results_grid += [[1 for i in range(len(tree_grid[1]))]]
     brute_force_visibility_scan(tree_grid, results_grid)
