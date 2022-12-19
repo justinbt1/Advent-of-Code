ed591049624497d8a1c529f72dc22c6f60164f2d
Justin <85031828+justinbt1@users.noreply.github.com>
2022-12-19 09:33:19 +0000

    Add partially completed day 9 code

--- 2022/day_9-rope_bridge.py
+++ 2022/day_9-rope_bridge.py
@@ -1,51 +1,72 @@
-
-
-if __name__ == '__main__':
-    tracker = [(0, 0)]
-    head_x = 0
-    head_y = 0
-    tail_x = 0
-    tail_y = 0
-
+def read_motions():
     with open('data/9.txt', 'rt') as input_file:
         motions = []
         for motion in input_file:
             motion_details = motion.strip().split()
             motions.append((motion_details[0], int(motion_details[1])))
 
+    return motions
+
+
+def shift_knots(rope):
+    for i in range(len(rope) - 1):
+        previous = rope[i]
+        current = rope[i + 1]
+
+        if (previous[0] - current[0]) > 1:
+            current[0] += 1
+            current[1] = previous[1]
+
+        elif (current[0] - previous[0]) > 1:
+            current[0] -= 1
+            current[1] = previous[1]
+
+        elif (previous[1] - current[1]) > 1:
+            current[1] += 1
+            current[0] = previous[0]
+
+        elif (current[1] - previous[1]) > 1:
+            current[1] -= 1
+            current[0] = previous[0]
+
+    return rope
+
+
+def test(n):
+    tracker = [(0, 0)]
+    rope = [[0, 0] for i in range(n)]
+    motions = read_motions()
+
     for motion in motions:
         if motion[0] == 'R':
-            for i in range(motion[1]):
-                head_x += 1
-                if (head_x - tail_x) > 1:
-                    tail_x += 1
-                    tail_y = head_y
-                    tracker.append((tail_x, tail_y))
+            for _ in range(motion[1]):
+                rope[0][0] += 1
+                rope = shift_knots(rope)
+                tracker.append(tuple(rope[-1]))
 
         elif motion[0] == 'L':
-            for i in range(motion[1]):
-                head_x -= 1
-                if (tail_x - head_x) > 1:
-                    tail_x -= 1
-                    tail_y = head_y
-                    tracker.append((tail_x, tail_y))
+            for _ in range(motion[1]):
+                rope[0][0] -= 1
+                rope = shift_knots(rope)
+                tracker.append(tuple(rope[-1]))
 
         elif motion[0] == 'U':
-            for i in range(motion[1]):
-                head_y += 1
-                if (head_y - tail_y) > 1:
-                    tail_y += 1
-                    tail_x = head_x
-                    tracker.append((tail_x, tail_y))
+            for _ in range(motion[1]):
+                rope[0][1] += 1
+                rope = shift_knots(rope)
+                tracker.append(tuple(rope[-1]))
 
         elif motion[0] == 'D':
-            for i in range(motion[1]):
-                head_y -= 1
-                if (tail_y - head_y) > 1:
-                    tail_y -= 1
-                    tail_x = head_x
-                    tracker.append((tail_x, tail_y))
+            for _ in range(motion[1]):
+                rope[0][1] -= 1
+                rope = shift_knots(rope)
+                tracker.append(tuple(rope[-1]))
 
-        print(motion, head_x, head_y, tail_x, tail_y)
+    return len(set(tracker))
 
-    print(f'Part 1: {len(set(tracker))}')
+
+if __name__ == '__main__':
+    tail_visits = test(2)
+    print(f'Part 1: {tail_visits}')
+    tail_visits = test(10)
+    print(f'Part 2: {tail_visits}')
