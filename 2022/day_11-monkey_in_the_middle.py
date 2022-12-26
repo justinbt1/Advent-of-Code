58d3f4875cc30030e1e1f0e4377176806faba0a7
Justin <85031828+justinbt1@users.noreply.github.com>
2022-12-26 14:08:37 +0000

    Added Monkey class stub

--- 2022/day_11-monkey_in_the_middle.py
+++ 2022/day_11-monkey_in_the_middle.py
@@ -1,6 +1,12 @@
+import json
+
 
 class Monkey:
     pass
 
+
 if __name__ == "__main__":
-    pass
+    with open('data/10.txt', 'rt') as input_file:
+        monkey_data = json.load(input_file)
+
+    print(monkey_data['Monkey 0'])
