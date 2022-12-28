c108b6d6145c2d32d630ea5b99a30da9e3ea7c82
Justin <85031828+justinbt1@users.noreply.github.com>
2022-12-28 13:05:02 +0000

    Partially completed code for day 11 part 2

--- 2022/day_11-monkey_in_the_middle.py
+++ 2022/day_11-monkey_in_the_middle.py
@@ -1,47 +1,35 @@
+import math
+
 
 class Monkey:
     def __init__(self):
         self.items = None
         self.operation = None
-        self.test = None
+        self.divisor = None
         self.if_true = None
         self.if_false = None
         self.inspection_count = 0
 
-    def inspect_item(self):
+    def inspect_item(self, mod):
         self.inspection_count += 1
-        item = self.items.pop(0)
+        item = int(self.items.pop(0)) % mod
         if self.operation[0] == '*':
             if self.operation[-1] == 'old':
-                item = int(item) * int(item)
+                item = item * item
             else:
-                item = int(item) * int(self.operation[-1])
+                item = item * int(self.operation[-1])
         else:
             if self.operation[-1] == 'old':
-                item = int(item) * int(item)
+                item = item * item
             else:
-                item = int(item) + int(self.operation[-1])
+                item = item + int(self.operation[-1])
         return item
 
     def next_monkey(self, item):
-        if not int(item) % int(self.test):
+        if not item % int(self.divisor):
             return int(self.if_true)
         else:
             return int(self.if_false)
-
-
-def conduct_rounds(monkeys, n, relief=True):
-    for i in range(n):
-        print(f'Round: {i}')
-        for monkey in monkeys:
-            for _ in range(len(monkey.items)):
-                item = monkey.inspect_item()
-                if relief:
-                    item = item // 3
-                next_monkey = monkey.next_monkey(item)
-                monkeys[next_monkey].items.append(item)
-
-    return monkeys
 
 
 def read_data(data_file):
@@ -58,8 +46,8 @@ def read_data(data_file):
                 monkey.items = [item.replace(',', '') for item in items]
             case ['Operation:', *operation]:
                 monkey.operation = operation[-2:]
-            case ['Test:', *test]:
-                monkey.test = test[-1]
+            case ['Test:', *divisor]:
+                monkey.divisor = divisor[-1]
             case ['If', 'true:', *recieving_monkey]:
                 monkey.if_true = recieving_monkey[-1]
             case ['If', 'false:', *recieving_monkey]:
@@ -70,14 +58,30 @@ def read_data(data_file):
     return monkeys
 
 
-def find_top_monkey_business(monkey_data, n_rounds, relief=False):
-    monkeys = conduct_rounds(monkey_data, n_rounds, relief=relief)
+def conduct_rounds(monkeys, n, part):
+    modulus = math.lcm(*[int(monkey.divisor) for monkey in monkeys])
+    for i in range(n):
+        for monkey in monkeys:
+            for _ in range(len(monkey.items)):
+                item = monkey.inspect_item(modulus)
+                if part == 1:
+                    item = item // 3
+                next_monkey = monkey.next_monkey(item)
+                monkeys[next_monkey].items.append(item)
 
-    inspections = []
-    for monkey in monkeys:
-        inspections.append(monkey.inspection_count)
+        if i + 1 in [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]:
+            print(f'Round: {i}')
+            l = []
+            for j, monkey in enumerate(monkeys):
+                l.append(monkey.inspection_count)
+            print(l)
 
-    inspections.sort()
+    return monkeys
+
+
+def find_top_monkey_business(monkey_data, n_rounds, part):
+    monkeys = conduct_rounds(monkey_data, n_rounds, part=part)
+    inspections = sorted([monkey.inspection_count for monkey in monkeys])
     monkey_business = inspections[-2] * inspections[-1]
     print(monkey_business)
 
@@ -85,5 +89,5 @@ def find_top_monkey_business(monkey_data, n_rounds, relief=False):
 if __name__ == "__main__":
     with open('data/11.txt', 'rt') as input_file:
         monkey_data = read_data(input_file)
-        find_top_monkey_business(monkey_data, 20, True)
-        # find_top_monkey_business(monkey_data, 1000, False)
+        find_top_monkey_business(monkey_data, 20, part=1)
+        find_top_monkey_business(monkey_data, 10000, part=2)
