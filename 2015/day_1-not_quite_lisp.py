c9093b1bcfc536ad868257ef07e1ec9a6a0b8d92
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-02-15 14:19:18 +0000

    2015 day 1 part 2

--- 2015/day_1-not_quite_lisp.py
+++ 2015/day_1-not_quite_lisp.py
@@ -1,18 +1,31 @@
+def move_floor(command):
+    if command == '(':
+        return 1
+    else:
+        return -1
+
 
 def what_floor(instructions):
     floor = 0
     for instruction in instructions:
-        if instruction == '(':
-            floor += 1
-        else:
-            floor -= 1
+        floor += move_floor(instruction)
 
     print(floor)
 
+def when_basement(instructions):
+    floor = 0
+    for i, instruction in enumerate(instructions):
+        floor += move_floor(instruction)
+        if floor < 0:
+            break
+
+    print(i)
 
 
 if __name__ == '__main__':
+    floor = 0
     with open('2015/data/1.txt', 'rt') as input_file:
         data = input_file.read().strip()
-        print(data)
         what_floor(data)
+        when_basement(data)
+
