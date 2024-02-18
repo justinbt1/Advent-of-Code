e7cfc4fd31ca90899bb74e777b957c78070e02b9
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-02-18 11:25:44 +0000

    2020 day 1 part 1

---
+++ 2020/day_1-report_repair.py
@@ -0,0 +1,13 @@
+def part_one(expenses):
+    for value in expenses:
+        for expense in expenses:
+            if value + expense == 2020:
+                return value * expense
+
+
+
+if __name__ == '__main__':
+    with open('2020/data/1.txt', 'rt') as file:
+        lines = [int(line.strip()) for line in file]
+
+    print(part_one(lines))
