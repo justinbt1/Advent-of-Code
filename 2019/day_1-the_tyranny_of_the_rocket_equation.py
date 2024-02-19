32ad4c06cc93c96125ba1eea2e8a49b37203683c
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-02-19 10:07:08 +0000

    2019 day 1 part 2

--- 2019/day_1-the_tyranny_of_the_rocket_equation.py
+++ 2019/day_1-the_tyranny_of_the_rocket_equation.py
@@ -1,5 +1,5 @@
 
-def fuel_adder_upper(mass, recursive=False):
+def fuel_adder_upper(mass, recursive):
     fuel_amount = mass // 3 - 2
 
     if not recursive:
@@ -8,14 +8,14 @@ def fuel_adder_upper(mass, recursive=False):
     if fuel_amount < 0:
         return 0
     else:
-        fuel_amount += fuel_adder_upper(fuel_amount)
+        fuel_amount += fuel_adder_upper(fuel_amount, recursive)
         return fuel_amount
 
 
 def get_fuel_requirement(module_masses, recursive=False):
     fuel = 0
     for mass in module_masses:
-        print(fuel_adder_upper(mass, recursive=recursive))
+        fuel += fuel_adder_upper(mass, recursive=recursive)
 
     return fuel
 
@@ -24,5 +24,5 @@ if __name__ == '__main__':
     with open('2019/data/1.txt', 'rt') as file:
         module_masses = [int(line.strip()) for line in file]
 
-    # print(get_fuel_requirement(module_masses))
+    print(get_fuel_requirement(module_masses))
     print(get_fuel_requirement(module_masses, recursive=True))
