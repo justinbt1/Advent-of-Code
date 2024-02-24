42459f5bc2b5911b976964e52c2c954f9f4d7624
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-02-24 21:59:24 +0000

    2018 day 1 part 2

--- 2018/day_1-chronal_calibration.py
+++ 2018/day_1-chronal_calibration.py
@@ -1,3 +1,5 @@
+from itertools import cycle
+
 
 def part_one(frequency_changes):
     current_frequency = 0
@@ -7,8 +9,22 @@ def part_one(frequency_changes):
     print(current_frequency)
 
 
+def part_two(frequency_changes):
+    previous_frequencies = {0}
+    current_frequency = 0
+    for change in cycle(frequency_changes):
+        current_frequency += change
+        
+        if current_frequency in previous_frequencies:
+            print(current_frequency)
+            break
+            
+        previous_frequencies.add(current_frequency)
+
+
 if __name__ == '__main__':
     with open('2018/data/1.txt', 'rt') as file:
         frequencies = [int(line.strip()) for line in file]
 
     part_one(frequencies)
+    part_two(frequencies)
