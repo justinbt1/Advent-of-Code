efe752c2944effad69a18e1e17ec9097dfa1740d
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-02-29 17:45:59 +0000

    Partial 2016 day 1 part 1

---
+++ 2016/no_time_for_a_taxicab.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class Location:
+    def __init__(self):
+        self.coords = np.array((0, 0))
+        self._i = 0
+
+    def re_orient(self, turn):
+        orientations = np.array([(0, -1), (-1, 0), (0, 1), (1, 0)])
+        i = 3 if turn == 'L' else 1
+        self._i = (self._i + i) % len(orientations)
+
+        return orientations[self._i]
+
+    def walk(self, direction):
+        orientation = self.re_orient(direction[0])
+        new_location = orientation * int(direction[1])
+        self.coords += new_location
+
+
+if __name__ == '__main__':
+    with open('2016/data/1.txt', 'rt') as file:
+        instructions = file.read().strip().split(', ')
+        instructions = [list(d) for d in instructions]
+
+    location = Location()
+    for instruction in instructions:
+        location.walk(instruction)
+
+    print(abs(location.coords.sum()))
