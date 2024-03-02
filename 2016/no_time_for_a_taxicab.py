09b9dc3b1920c065033b26773c2bbe2f2395548a
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-03-02 23:01:45 +0000

    Partial 2016 day 1 part 1

--- 2016/no_time_for_a_taxicab.py
+++ 2016/no_time_for_a_taxicab.py
@@ -9,7 +9,7 @@ class Location:
     def re_orient(self, turn):
         orientations = np.array([(0, -1), (-1, 0), (0, 1), (1, 0)])
         i = 3 if turn == 'L' else 1
-        self._i = (self._i + i) % len(orientations)
+        self._i = (self._i + i) % 4
 
         return orientations[self._i]
 
@@ -18,14 +18,21 @@ class Location:
         new_location = orientation * int(direction[1])
         self.coords += new_location
 
+    def get_distance(self):
+        print(np.abs(self.coords).sum())
+
+
+def find_bunny_hq(instructions):
+    location = Location()
+    for instruction in instructions:
+        location.walk(instruction)
+
+    location.get_distance()
+
 
 if __name__ == '__main__':
     with open('2016/data/1.txt', 'rt') as file:
         instructions = file.read().strip().split(', ')
         instructions = [list(d) for d in instructions]
 
-    location = Location()
-    for instruction in instructions:
-        location.walk(instruction)
-
-    print(abs(location.coords.sum()))
+    find_bunny_hq(instructions)
