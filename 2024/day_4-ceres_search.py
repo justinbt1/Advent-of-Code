2122a5b57a5b2643d663a041e79a1f66d2f51d55
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-12-12 14:21:33 +0000

    2024 day 4 part 1

--- 2024/day_4-ceres_search.py
+++ 2024/day_4-ceres_search.py
@@ -1,8 +1,29 @@
 import numpy as np
 
 
-def find_christmas(matrix):
-    
+def find_christmas(matrix, sub_string):
+    occurs = 0
+    sub_string = [sub_string, sub_string[::-1]]
+    for m in range(len(matrix)):
+        for n in range(len(matrix[0])):
+            if ''.join(matrix[m, n:n + 4]) in sub_string:
+                occurs += 1
+            if ''.join(matrix[m:m + 4, n]) in sub_string:
+                occurs += 1
+            try:
+                if ''.join([matrix[m,n], matrix[m+1,n+1], matrix[m+2,n+2], matrix[m+3,n+3]]) in sub_string:
+                    occurs += 1
+            except IndexError:
+                pass
+            try:
+                if n - 3 < 0:
+                    continue
+                elif ''.join([matrix[m,n], matrix[m+1,n-1], matrix[m+2,n-2], matrix[m+3,n-3]]) in sub_string:
+                    occurs += 1
+            except IndexError:
+                pass
+
+    print(occurs)
 
 
 if __name__ == '__main__':
@@ -11,6 +32,5 @@ if __name__ == '__main__':
         for row in file:
             search_matrix.append((list(row.rstrip())))
 
-    print(np.array(search_matrix))
-
-    #find_christmas(search_matrix)
+    search_matrix = np.array(search_matrix)
+    find_christmas(search_matrix, 'XMAS')
