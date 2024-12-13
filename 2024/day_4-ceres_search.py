4ebb7bb6aa3a6e5e4ddd8d031be25008f5bfedd9
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2024-12-13 12:21:21 +0000

    2024 day 4 part 2

--- 2024/day_4-ceres_search.py
+++ 2024/day_4-ceres_search.py
@@ -10,18 +10,33 @@ def find_christmas(matrix, sub_string):
                 occurs += 1
             if ''.join(matrix[m:m + 4, n]) in sub_string:
                 occurs += 1
-            try:
-                if ''.join([matrix[m,n], matrix[m+1,n+1], matrix[m+2,n+2], matrix[m+3,n+3]]) in sub_string:
+            if m + 3 >= len(matrix):
+                continue
+            if n + 3 < len(matrix[0]):
+                if ''.join([matrix[m+i, n+i] for i in [*range(0, 4)]]) in sub_string:
                     occurs += 1
-            except IndexError:
-                pass
-            try:
-                if n - 3 < 0:
-                    continue
-                elif ''.join([matrix[m,n], matrix[m+1,n-1], matrix[m+2,n-2], matrix[m+3,n-3]]) in sub_string:
+            if n - 3 >= 0:
+                if ''.join([matrix[m+i, n-i] for i in [*range(0, 4)]]) in sub_string:
                     occurs += 1
-            except IndexError:
-                pass
+
+    print(occurs)
+
+
+def find_x_mas(matrix):
+    occurs = 0
+    sub_string = ['MASMS', 'SAMMS', 'MASSM', 'SAMSM']
+    for m in range(len(matrix) - 2):
+        for n in range(len(matrix[0]) - 2):
+            chars = [
+                matrix[m,n],
+                matrix[m+1,n+1],
+                matrix[m+2,n+2],
+                matrix[m+2,n],
+                matrix[m,n+2]
+            ]
+
+            if ''.join(chars) in sub_string:
+                occurs += 1
 
     print(occurs)
 
@@ -34,3 +49,4 @@ if __name__ == '__main__':
 
     search_matrix = np.array(search_matrix)
     find_christmas(search_matrix, 'XMAS')
+    find_x_mas(search_matrix)
