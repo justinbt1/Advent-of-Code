a920ebf66d2e14abe5ab1e6626b2400469021ec7
justinbt1 <85031828+justinbt1@users.noreply.github.com>
2025-12-01 18:03:14 +0000

    2025 day 1 part 2

--- 2025/1_secret_enterance.py
+++ 2025/1_secret_enterance.py
@@ -1,42 +1,34 @@
 
 def read_rotations():
-    rotations = []
     with open('2025/data/1.txt', 'rt') as rotation_buffer:
-        for instruction in rotation_buffer:
-            rotation = int(instruction[1:])
-            if instruction[0] == 'L':
-                rotation = -rotation
-
-            rotations.append(rotation)
-
-    return rotations
+    
+        return [(int(i[1:]), i[0]) for i in rotation_buffer]
 
 
-def zero_count(rotations, dial_max=99):
-    dial_position = 50
+def zero_count(instructions, dial_max=99, dial_position=50):
     zero_count = 0
-    for r in rotations:
-        dial_position = (dial_position + r) % (dial_max + 1)
+    for distance, direction in instructions:
+        i = -distance if direction == 'L' else distance
+        dial_position = (dial_position + i) % (dial_max + 1)
         if dial_position == 0:
             zero_count += 1
 
     print(zero_count)
 
 
-def zero_point_count(rotations, dial_max=99):
-    dial_position = 50
+def zero_point_count(instructions, dial_max=99, dial_position=50):
     zero_count = 0
-    for rotation in rotations:
-        for r in range(0, rotation, -1):
-            dial_position = (dial_position + r) % (dial_max + 1)
-            print(dial_position)
-
-        break
-
+    for distance, direction in instructions:
+        for _ in range(distance):
+            i = -1 if direction == 'L' else 1
+            dial_position = (dial_position + i) % (dial_max + 1)
+            if dial_position == 0:
+                zero_count += 1
+    
     print(zero_count)
 
 
 if __name__ == '__main__':
     rotations = read_rotations()
-    # zero_count(rotations)
+    zero_count(rotations)
     zero_point_count(rotations)
